dbff7fce060ddd8e0a14d1be4a572e68a5b00b13
kkuwa <takuwa@procube.jp>
2017-11-24 18:36:37 +0900

    raw body support

--- src/www/global.js
+++ src/www/global.js
@@ -11,6 +11,12 @@ var app = express();
 app.use(logger('combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.raw({
+  "type" : [
+    "application/octet-stream",
+    "text/xml",
+    "text/xml; Charset=utf-8"
+  ]}));
 app.use(cookieParser());
 app.use(helmet());
 
