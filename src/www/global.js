ee2d74263f5d7b7d6043bfe13d79eb771c2c35a7
kkuwa <takuwa@procube.jp>
2018-01-23 20:20:19 +0900

    Add compression

--- src/www/global.js
+++ src/www/global.js
@@ -5,10 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var helmet = require('helmet');
+var compression = require('compression');
 
 var app = express();
 
 var limit = '30mb';
+app.use(compression());
 app.use(logger('combined'));
 app.use(bodyParser.json({limit}));
 app.use(bodyParser.urlencoded({ extended: false, limit }));
