110a9a9a2a2855174b51ce0207dcc4ebd16b7984
kkuwa <takuwa@procube.jp>
2018-01-25 15:44:37 +0900

    Enable compression for mounted app's response

--- src/www/global.js
+++ src/www/global.js
@@ -6,11 +6,23 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var helmet = require('helmet');
 var compression = require('compression');
+var compressible = require('compressible')
 
 var app = express();
 
 var limit = '30mb';
-app.use(compression());
+app.use(compression({
+  filter : function(req, res) {
+    var ret = false;
+    var type = res.getHeader('Content-Type')
+    if (typeof type === "string" && compressible(type)) {
+      ret = true;
+    } else if (Array.isArray(type) && typeof type[0] === "string" && compressible(type[0])) {
+      ret = true;
+    }
+    return ret;
+  }
+}));
 app.use(logger('combined'));
 app.use(bodyParser.json({limit}));
 app.use(bodyParser.urlencoded({ extended: false, limit }));
